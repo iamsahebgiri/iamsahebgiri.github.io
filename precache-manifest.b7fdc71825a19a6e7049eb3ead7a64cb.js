de4c96265f59ed2650112267a532a5f9a56a7dda
iamsahebgiri <iamsahebgiri@gmail.com>
2019-04-22 16:33:17 +0530

    Initial Commit

---
+++ precache-manifest.b7fdc71825a19a6e7049eb3ead7a64cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "95fef60db00fbb1dec63",
+    "url": "/js/chunk-vendors.47350325.js"
+  },
+  {
+    "revision": "6cc5270f1e4ae85103d1",
+    "url": "/js/app.9e35f566.js"
+  },
+  {
+    "revision": "0c1ff6d3ac4d89b404afd664f65e52a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f186c73d8d12b2af94bdcdb12a4b0472",
+    "url": "/img/undraw_not_found.f186c73d.svg"
+  },
+  {
+    "revision": "78e1da397f45eb94869f5103060b2d53",
+    "url": "/img/snail-unicorn.78e1da39.svg"
+  },
+  {
+    "revision": "6a38a17bba885f6995880d486c14b64d",
+    "url": "/img/logo.6a38a17b.svg"
+  },
+  {
+    "revision": "6cc5270f1e4ae85103d1",
+    "url": "/css/app.edf07204.css"
+  }
+];
